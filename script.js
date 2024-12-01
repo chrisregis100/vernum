62f5f93bf968b821e64b62fa3f24f2d8c02a79ea
Chris2400 <erneregis@gmail.com>
2024-12-01 22:08:51 +0100

    update style file

--- script.js
+++ script.js
@@ -18,22 +18,27 @@ function processVCF(e) {
     reader.onload = function (event) {
         const vcfData = event.target.result;
         const updatedVCF = vcfData.split('\n').map(line => {
-          // Modifier les lignes contenant les numéros béninois
+          // Modification les lignes contenant les numéros béninois
           if (line.startsWith('TEL;CELL')) {
             const parts = line.split(':');
             const number = parts[1].trim();
 
             // Vérifier si le numéro commence par +229 et contient 8 chiffres
             if (/^\+229\d{8}$/.test(number)) {
-              const localNumber = number.slice(4); // Retirer +229
+              const localNumber = number.slice(4); 
               if (/^\d{8}$/.test(localNumber)) {
-                parts[1] = `+22901${localNumber}`; // Ajouter "01" après +229
+                parts[1] = `+22901${localNumber}`; 
                 return parts.join(':');
               }
+
+              // normalisation des numéros mal formatés
+            }
+            else if (/^\d{3}-\d{3}-\d{2}$/.test(number)) {
+                parts[1] = `+22901${number.replace(/-/g, '')}`; 
             }
           }
           
-          return line; // Retourner la ligne inchangée si ce n'est pas un numéro à modifier
+          return line; 
         }).join('\n');
 
         // Générer un nouveau fichier VCF
@@ -42,8 +47,6 @@ function processVCF(e) {
         downloadLink.href = url;
         downloadLink.classList.remove('hidden');
       };
-
-      reader.readAsText(file);
 
     reader.readAsText(file);
   }
